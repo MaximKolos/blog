4d0c576837d747870169331205c5833bbf762da8
MaximKolos <40358318+MaximKolos@users.noreply.github.com>
2018-06-27 13:21:52 +0300

    project
    blog .NetBlog

---
+++ GuestResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# blog
+# WebApplication1
